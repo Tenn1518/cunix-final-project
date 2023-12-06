9189759fae5c6139696e954a71c2b2d840d811fa
Brandon Adams <brandontadamss@gmail.com>
2023-12-05 19:11:02 -0500

    teacher header file update

--- teacher.h
+++ teacher.h
@@ -1,4 +1,14 @@
 #ifndef TEACHER_H_
 #define TEACHER_H_
 
+#include "data.h"
+
+// Function declarations for teacher-related operations
+void create_student(Student students[], int index);
+void edit_student_grades(Student students[], int students_index);
+void view_student_grades(Student students[], int students_index);
+
+// Other declarations related to teachers if needed
+
 #endif // TEACHER_H_
+
